845027a6c238e289eee99d51f94dbad7ed3f925c
dolarson <David.Larson@nelnet.net>
2016-10-13 10:46:03 -0500

    Kata 1 - Dice

---
+++ David/Kata 1 - Dice/Main.java
@@ -0,0 +1,7 @@
+package main;
+
+public class Main{
+	public static void main(String[] args){
+		System.out.println((new java.util.Random().nextInt(6) + 1) + " " + (new java.util.Random().nextInt(6) + 1));
+	}
+}
